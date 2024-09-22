5e9e8ea8eac1d4802255f093d04b8bf0e9dd15f9
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-09-22 11:18:43 +0300

    fix home add img

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,14 +2,20 @@ import { Link, useLocation } from 'react-router-dom';
 import css from './MovieList.module.css'
 
 const MovieList = ({ trendMovies }) => {
+    console.log(trendMovies);
     const location = useLocation();
     return (
         <>
 
             <ul className={css.listContainer}>
-                {trendMovies.map(({ id, title }) => (
+                {trendMovies.map(({ id, title, poster_path }) => (
                     <li key={id} className={css.listItem}>
                         <Link to={`/movies/${id}`} state={location} className={css.listLink}>
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500/${poster_path}?language=en-US&include_image_language=en,null`}
+                                alt={name}
+                                className={css.listImg}
+                            />
                             {title}
                         </Link>
                     </li>
